eecabca802456edcd84878bfd7214778247ef8f3
kubaxd8641 <jakubsobczyk484@gmail.com>
2026-01-30 15:13:17 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./index.js";
 import { initSocket } from "./config/socket.js";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 initSocket(server);
 
